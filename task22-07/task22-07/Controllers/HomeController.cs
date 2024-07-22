b68f62913a87a4a035f8c7f3074364d3de4bbafa
Ayahaldomi <ayahaldomi@gmail.com>
2024-07-22 17:57:20 +0300

    task

---
+++ task22-07/task22-07/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task22_07.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "this is my second page";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "this is my third page";
+
+            return View();
+        }
+        public ActionResult services()
+        {
+            ViewBag.Message = "this is my fourth page";
+
+            return View();
+        }
+        public ActionResult results()
+        {
+            ViewBag.Message = "this is my third page";
+
+            return View();
+        }
+    }
+}
